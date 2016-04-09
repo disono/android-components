2d27e5663e98bc3e4b1cd287154939dfabdd4570
Archie <disonoarchie@gmail.com>
2016-04-09 17:54:30 +0800

    Added contextual toolbar

--- Toolbar/app/src/main/java/com/webmons/disono/toolbar/HomeFragment.java
+++ Toolbar/app/src/main/java/com/webmons/disono/toolbar/HomeFragment.java
@@ -5,15 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    View inflaterView;
+
+    TextView textView_home;
+    Menu optionsMenu;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,28 +31,23 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        inflaterView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        String unixTime = "Unix Time: " + System.currentTimeMillis() / 1000L;
+        textView_home = (TextView) inflaterView.findViewById(R.id.textView_home);
+        textView_home.setText(unixTime);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflaterView;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.fragment_home, menu);
-    }
+        optionsMenu = menu;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_refresh) {
-            Toast.makeText(this.getContext(), "Refresh", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.action_settings) {
-            Toast.makeText(this.getContext(), "Settings", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        // add new item on menu
+        menu.add(0, 1, 0, "Option 1");
     }
 
 }
--- Toolbar/app/src/main/java/com/webmons/disono/toolbar/MainActivity.java
+++ Toolbar/app/src/main/java/com/webmons/disono/toolbar/MainActivity.java
@@ -7,9 +7,13 @@ package com.webmons.disono.toolbar;
  * @lincense The MIT License (MIT)
  */
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,10 +22,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -35,19 +44,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         ActionBar actionBar = getSupportActionBar();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
         switch (item.getItemId()) {
             case R.id.action_refresh:
                 Toast.makeText(MainActivity.this, "Refresh", Toast.LENGTH_SHORT).show();
+                HomeFragment homeFragment = new HomeFragment();
 
-                getSupportFragmentManager()
+                // add the fragment
+                fragmentManager
                         .beginTransaction()
-                        .add(R.id.activityParentView, new HomeFragment())
+                        .replace(R.id.activityParentView, homeFragment)
+                        .addToBackStack(null) // add to history stack
                         .commit();
+
+                // ensure all transactions is finished
+                fragmentManager.executePendingTransactions();
 
                 break;
             case R.id.action_settings:
                 Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+
+                ContextualToolbar contextualToolbar = new ContextualToolbar();
+
+                // add the fragment
+                fragmentManager
+                        .beginTransaction()
+                        .replace(R.id.activityParentView, contextualToolbar)
+                        .addToBackStack(null) // add to history stack
+                        .commit();
+
+                // ensure all transactions is finished
+                fragmentManager.executePendingTransactions();
+
                 break;
             case R.id.show_toolbar:
                 // show actionbar
@@ -83,12 +112,25 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onBackPressed() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (fragmentManager.getBackStackEntryCount() > 0) {
+            // back to history stack
+            fragmentManager.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     // using immersive full screen mode
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     private void hideSystemUI() {
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // hide nav bar
-                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                | View.SYSTEM_UI_FLAG_IMMERSIVE // remove the following flag for version < 19
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE // remove the following flag for version < 19
         );
     }
 }
